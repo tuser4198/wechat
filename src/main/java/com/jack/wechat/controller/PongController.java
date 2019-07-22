87fbd4ab82fb91fe0e49707da82fe857337230ff
chaijq <chaijq@hilife.com>
2019-07-22 19:33:57 +0800

    test emoji

--- src/main/java/com/jack/wechat/controller/PongController.java
+++ src/main/java/com/jack/wechat/controller/PongController.java
@@ -7,7 +7,7 @@ import com.jack.wechat.utils.JsonUtil;
 import com.jack.wechat.utils.LocalDateTimeUtil;
 import com.jack.wechat.utils.MessageUtil;
 import com.jack.wechat.utils.SignUtil;
-import com.vdurmont.emoji.EmojiParser;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,7 +55,8 @@ public class PongController {
         //        msg.setContent(EmojiParser.parseToUnicode(msg.getContent()));
         msgMapper.insert(msg);
         Msg dbMsg = msgMapper.selectById(msg.getId());
-        dbMsg.setContent(EmojiParser.parseToUnicode(dbMsg.getContent()));
+        //        dbMsg.setContent(EmojiParser.parseToUnicode(dbMsg.getContent()));
+        dbMsg.setContent(filterEmoji(dbMsg.getContent()));
         lastMsg = JsonUtil.toJSon(dbMsg);
         LOGGER.info(lastMsg);
     }
@@ -63,6 +64,81 @@ public class PongController {
     @GetMapping(value = "/getLastMsg")
     public String getLastMsg() {
         return lastMsg;
+    }
+
+    private static String filterEmoji(String source) {
+        if (StringUtils.isEmpty(source)) {
+            return "";
+        }
+        source = source.replace("[\\ud800\\udc00-\\udbff\\udfff\\ud800-\\udfff]", "")
+            .replace("??", "");
+        if (!containsEmoji(source)) {
+            return source; //如果不包含，直接返回
+        }
+        //到这里铁定包含
+        StringBuilder buf = null;
+
+        int len = source.length();
+
+        for (int i = 0; i < len; i++) {
+            char codePoint = source.charAt(i);
+
+            if (!isEmojiCharacter(codePoint)) {
+                if (buf == null) {
+                    buf = new StringBuilder(source.length());
+                }
+
+                buf.append(codePoint);
+            } else {
+            }
+        }
+
+        if (buf == null) {
+            return source; //如果没有找到 emoji表情，则返回源字符串
+        } else {
+            if (buf.length() == len) {
+                //这里的意义在于尽可能少的toString，因为会重新生成字符串
+                buf = null;
+                return source;
+            } else {
+                return buf.toString();
+            }
+        }
+    }
+
+    /**
+     * 检测是否有emoji字符
+     *
+     * @param source
+     * @return 一旦含有就抛出
+     */
+    public static boolean containsEmoji(String source) {
+        if (StringUtils.isBlank(source)) {
+            return false;
+        }
+
+        int len = source.length();
+
+        for (int i = 0; i < len; i++) {
+            char codePoint = source.charAt(i);
+
+            if (isEmojiCharacter(codePoint)) {
+                //do nothing，判断到了这里表明，确认有表情字符
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isEmojiCharacter(char codePoint) {
+        return (codePoint == 0x0) ||
+            (codePoint == 0x9) ||
+            (codePoint == 0xA) ||
+            (codePoint == 0xD) ||
+            ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
+            ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
+            ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF));
     }
 
 }
